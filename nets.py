5294d46ede30df3bbdb49d28a2c603bee3eb7e0c
Wei Dong <wdong@wdong.org>
2017-02-02 11:56:15 -0500

    update

--- nets.py
+++ nets.py
@@ -35,6 +35,35 @@ def tiny (X, num_classes=2):
     net = tf.identity(net, 'logits')
     return net, 8
 
+def tiny2 (X, num_classes=2):
+    # stride is  2 * 2 * 2 * 2 = 16
+    net = X
+    layers = [X]
+    with tf.name_scope('simple'):
+        # slim.arg_scope([slim.conv2d]):
+        # slim.conv2d defaults:
+        #   padding = 'SAME'
+        #   activation_fn = nn.relu
+        # parameters: net, out_channels, kernel_size, stride
+        net = slim.batch_norm(slim.conv2d(net, 64, 3, 2, scope='conv1'))
+        net = slim.max_pool2d(net, 2, 2, scope='pool1')
+        net = slim.batch_norm(slim.conv2d(net, 128, 3, 1, scope='conv2_1'))
+        net = slim.batch_norm(slim.conv2d(net, 128, 3, 1, scope='conv2_2'))
+        net = slim.max_pool2d(net, 2, 2, scope='pool2')
+        net = slim.batch_norm(slim.conv2d(net, 256, 3, 1, scope='conv3_1'))
+        net = slim.batch_norm(slim.conv2d(net, 256, 3, 1, scope='conv3_2'))
+        net = slim.batch_norm(slim.conv2d(net, 128, 1, 1, scope='conv5'))
+        #net = slim.dropout(net, keep_prob=0.9, scope='dropout')
+        net = slim.batch_norm(slim.conv2d(net, 32, 1, 1, scope='conv6'))
+        net = slim.batch_norm(slim.conv2d_transpose(net, 16, 9, 4, scope='upscale1'))
+        net = slim.batch_norm(slim.conv2d_transpose(net, 8, 5, 2, scope='upscale2'))
+        net = slim.conv2d(net, num_classes, 3, 1, scope='final',
+                            activation_fn=None,
+                            normalizer_fn=None,
+                         )
+    net = tf.identity(net, 'logits')
+    return net, 8
+
 def woshialex (X, num_classes=2):
     net = X
     with tf.name_scope('woshialex'):
